a05f50217545ef2c17eb6b2c04764cff44d119a3
shaya <shayaharuno@gmail.com>
2026-04-22 10:34:57 +0200

    Singleton order issue fixed

--- addons/sh_utils/debug/debug.gd
+++ addons/sh_utils/debug/debug.gd
@@ -18,7 +18,7 @@ var debug_overlay: Control
 
 func _ready() -> void:
 	if OS.is_debug_build():
-		set_debug_keyboard_actions()
+		register_debug_keyboard_actions()
 
 		debug_overlay = DEBUG_OVERLAY.instantiate()
 		debug_overlay.visible = show_debug_overlay
@@ -26,27 +26,27 @@ func _ready() -> void:
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("sh_toggle_debug_overlay"):
-		show_debug_overlay = !show_debug_overlay
-		debug_overlay.visible = show_debug_overlay
+	if OS.is_debug_build():
+		if event.is_action_pressed("sh_toggle_debug_overlay"):
+			show_debug_overlay = !show_debug_overlay
+			debug_overlay.visible = show_debug_overlay
 
-	if show_debug_overlay or debug_shortcuts_without_overlay:
-		if event.is_action_pressed("sh_force_quit"):
-			debug_force_quit.emit()
-		if event.is_action_pressed("sh_force_reload_scene"):
-			get_tree().reload_current_scene()
-		if event.is_action_pressed("sh_toggle_vsync"):
-			debug_toggle_vsync.emit()
-		if event.is_action_pressed("sh_toggle_fullscreen"):
-			debug_toggle_fullscreen.emit()
-		if event.is_action_pressed("sh_capture_screenshot"):
-			capture_screenshot()
+		if show_debug_overlay or debug_shortcuts_without_overlay:
+			if event.is_action_pressed("sh_force_quit"):
+				debug_force_quit.emit()
+			if event.is_action_pressed("sh_force_reload_scene"):
+				get_tree().reload_current_scene()
+			if event.is_action_pressed("sh_toggle_vsync"):
+				debug_toggle_vsync.emit()
+			if event.is_action_pressed("sh_toggle_fullscreen"):
+				debug_toggle_fullscreen.emit()
+			if event.is_action_pressed("sh_capture_screenshot"):
+				capture_screenshot()
 
 
 func get_hardware_info() -> Dictionary:
-	var _gpu_type: String
+	var _gpu_type: String = "Unknown"
 	match RenderingServer.get_video_adapter_type():
-		RenderingDevice.DEVICE_TYPE_OTHER: _gpu_type = "Unknown"
 		RenderingDevice.DEVICE_TYPE_INTEGRATED_GPU: _gpu_type = "Integrated"
 		RenderingDevice.DEVICE_TYPE_DISCRETE_GPU: _gpu_type = "Discrete"
 		RenderingDevice.DEVICE_TYPE_VIRTUAL_GPU: _gpu_type = "Virtual"
@@ -54,8 +54,9 @@ func get_hardware_info() -> Dictionary:
 	return {
 		"Platform": "%s %s" % [OS.get_name(), OS.get_version()],
 		"CPU": "%s (%s)" % [OS.get_processor_name(), OS.get_processor_count()],
-		"GPU": "%s (%s)" % [RenderingServer.get_video_adapter_name(), _gpu_type],
-		"API version": "%s" % RenderingServer.get_video_adapter_api_version(),
+		"GPU": "%s" % [RenderingServer.get_video_adapter_name()],
+		"GPU Type": "%s" % [_gpu_type],
+		"API ver": "%s" % RenderingServer.get_video_adapter_api_version(),
 		"Rendering Driver": "%s" % [RenderingServer.get_current_rendering_driver_name()],
 		"RAM": "%.0f MB" % (OS.get_memory_info()["physical"] / 1048576),
 		"System Locale": "%s" % OS.get_locale_language(),
@@ -77,7 +78,7 @@ func get_performance_info() -> Dictionary:
 	}
 
 
-func set_debug_keyboard_actions() -> void:
+func register_debug_keyboard_actions() -> void:
 	if not InputMap.has_action("sh_force_quit"):
 		InputMap.add_action("sh_force_quit")
 		var event = InputEventKey.new()
--- addons/sh_utils/plugin.gd
+++ addons/sh_utils/plugin.gd
@@ -3,9 +3,12 @@ extends EditorPlugin
 
 
 func _enable_plugin() -> void:
+	## Order of adding singletons matters:
+		## Everything dependant on Utils
+		## Config dependant on Debug
 	add_autoload_singleton("Utils", "res://addons/sh_utils/utils/utils.gd")
-	add_autoload_singleton("Config", "res://addons/sh_utils/config/config.gd")
 	add_autoload_singleton("Debug", "res://addons/sh_utils/debug/debug.tscn")
+	add_autoload_singleton("Config", "res://addons/sh_utils/config/config.gd")
 
 
 func _disable_plugin() -> void:
